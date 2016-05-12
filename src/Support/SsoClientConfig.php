106df244e03039535a12b8560896df3ebc468133
jayxhj <jayxhj@gmail.com>
2016-05-12 18:16:59 +0800

    更新 SsoClientConfig  读取配置方式
    1. 通过 config/ssoclientconfig.php 数组的 key 来设置配置
    2. 添加 getConfigName 方法，通过 config/ssoclientconfig.php 数组的 key 获取调用 config 的 key 名

--- src/Support/SsoClientConfig.php
+++ src/Support/SsoClientConfig.php
@@ -11,15 +11,30 @@ class SsoClientConfig
     /**
      *
      * 根据 config 名设置对应应用的配置
-     * @param string $configName
+     * @param string $configKey
+     * @return void
      * */
-    public function setConfig($configName)
+    public function setConfig($configKey)
     {
-        $ssoConfig = config('ssoclientconfig.'.$configName);
-        if (! is_null(array_get($ssoConfig, 'path'))) {
-            $config = $this->getClientConfig(array_get($ssoConfig, 'path'));
-            \Config::set($configName, $config);
+        $ssoConfig = config('ssoclientconfig.'.$configKey);
+        if (! is_null(array_get($ssoConfig, 'path')) && ! \Config::offsetExists(array_get($ssoConfig, 'config_name'))) {
+            $config = $this->_getClientConfig(array_get($ssoConfig, 'path'));
+            \Config::set(array_get($ssoConfig, 'config_name'), $config);
         }
+    }
+
+    /**
+     * 通过 config_key 获取配置对应的配置名（供 config() 调用时使用）
+     * @param string $configKey
+     * @return mixed
+     *
+     * */
+    public function getConfigName($configKey)
+    {
+        $ssoConfig = config('ssoclientconfig.'.$configKey);
+
+        return array_get($ssoConfig, 'config_name');
+
     }
 
     /**
@@ -28,7 +43,7 @@ class SsoClientConfig
      * @return array
      *
      * */
-    private function getClientConfig($path)
+    private function _getClientConfig($path)
     {
         if (file_exists($path)) {
             return include $path;
