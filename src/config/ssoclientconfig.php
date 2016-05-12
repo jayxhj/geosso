a30fde6484c5841145d98657b6438b59db9ed4bb
jayxhj <jayxhj@gmail.com>
2016-05-12 19:53:51 +0800

    修改 config/ssoclientconfig.php 配置
    1. config_name 不需与 key 相同，config() 获取配置时的 key 为 config_name

--- src/config/ssoclientconfig.php
+++ src/config/ssoclientconfig.php
@@ -7,6 +7,11 @@ return [
     'ana'       => [
         'path'        => config_path('ssoclient.php'),
         'domain'      => 'ana.geotmt.com',
-        'config_name' => 'ana',
+        'config_name' => 'ana_sso',
+    ],
+    'marketing' => [
+        'path'        => config_path('marketing_sso.php'),
+        'domain'      => 'mp.geotmt.com',
+        'config_name' => 'marketing_sso',
     ],
 ];
